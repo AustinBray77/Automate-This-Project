2cf1adf3152609b29fd6b13d86335bdfae1e857b
AustinBray77 <austin.bray77@gmail.com>
2023-10-27 17:49:56 -0400

    First Background Added

--- src/BackgroundSlides.elm
+++ src/BackgroundSlides.elm
@@ -7,6 +7,36 @@ import SlideUtilTypes exposing (Msg)
 import Animations exposing (animate)
 import Animations exposing (rotateAni)
 
+firstBackground: Shape Msg
+firstBackground =
+  group
+  [  
+    roundedRect 135 1200 0 
+      |> filled (rgb 27 83 141)
+      |> move (-900, 0)
+      |> addOutline (solid  0 ) black
+    ,
+    ngon 6 100 
+      |> filled (rgb 209 80 67)
+      |> rotate (degrees -30 )
+      |> move (700, -214.59854014598525)
+    ,
+    ngon 6 150 
+      |> filled (rgb 64 143 225)
+      |> rotate (degrees -30 )
+      |> move (700, 375)
+    ,
+    ngon 6 50 
+      |> filled (rgb 221 213 32)
+      |> rotate (degrees -30 )
+      |> move (600, -375)
+    ,
+    ngon 6 250 
+      |> filled black
+      |> rotate (degrees -30 )
+      |> move (960, 60)
+  ]
+
 thirdBackground: Float -> Shape Msg
 thirdBackground time =
   group
